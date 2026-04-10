ffb8d5d309a38ddd446c99fc2e01b437c71ff659
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-04-10 15:12:27 +0000

    gkaqj

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,21 +7,31 @@ public class PracticeProblem {
 	public static void q1() {
 		//Write question 1 code here
 	}
+	public static String seriesInclusive(int shibby){
+		String nun = "1";
+		int num = 1;
+		if (shibby < 1){
+			return "";
+		}
+		while (num < shibby){
+			num++;
+			nun += " " + num;
+		}
 
-	public static void q2() {
-		//Write question 2 code here
+		return nun;
 	}
+	public static String seriesExclusive(int shibby){
+		String nun = "0";
+		int num = 0;
+		if (shibby < 1){
+			return "";
+		}
+		while (num < shibby-1){
+			num++;
+			nun += " " + num;
+		}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+		return nun;
 	}
 
 }
